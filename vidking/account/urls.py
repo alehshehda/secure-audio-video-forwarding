5280aa957458115ae1b3e0b0dd0a99916c059110
Oleg <aleh.shehda@gmail.com>
2024-11-06 18:56:32 +0100

    Created database and 2 html files

--- vidking/account/urls.py
+++ vidking/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import ProtectedView, LoginView, LogoutView, CustomRefreshTokenView, LoginPageView
+from .views import ProtectedView, LoginView, LogoutView, CustomRefreshTokenView, LoginPageView, upload_success
+from django.urls import path
+from .views import upload_file
 
 urlpatterns = [
     
@@ -12,9 +14,11 @@ urlpatterns = [
     path('login/', LoginPageView.as_view(), name='login'),
     path('api/token/refresh/', CustomRefreshTokenView.as_view(), name='token_refresh'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    #path('', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
+    path('upload/', upload_file, name='upload_file'),
+    path('success/', upload_success, name='upload_success'),
     
     
 ]
