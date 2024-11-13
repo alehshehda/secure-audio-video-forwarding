1acd18ee9e965ac154321e8387710dd603a0a3fb
alexeylapachukStudent <129609640+alexeylapachukStudent@users.noreply.github.com>
2024-11-13 13:35:43 +0100

    Ended JWT

--- vidking/account/urls.py
+++ vidking/account/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('dashboard/', ProtectedView.as_view(), name='user_dashboard'),
     path('register/', views.user_registration, name='register'),
     path('logout-that-login/', auth_views.logout_then_login, name='logout-than-login'),
-  
-  
-  
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/protected/', ProtectedView.as_view(), name='protected'),
     path('api/token/refresh/', CustomRefreshTokenView.as_view(), name='token_refresh'),
